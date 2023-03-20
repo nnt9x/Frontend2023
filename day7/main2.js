58b750bdf98e3f98fb36a6c11a145bad861e94a7
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-20 21:33:06 +0700

    update js
    Bai tap 4 chua hoan thien, luu y

---
+++ day7/main2.js
@@ -0,0 +1,12 @@
+// Gợi ý làm bài bóng đèn
+
+// Khối lệnh if (điều kiện )
+// Nếu đúng - thì
+
+var state = "off";
+if(state == "on"){
+    console.log("Tắt đèn");
+}
+if(state == "off"){
+    console.log("Bật đèn");
+}
