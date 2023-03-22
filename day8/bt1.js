97e2f9f52b093e9f5988074c597570d44b4a2e4e
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-22 20:29:49 +0700

    update day8

---
+++ day8/bt1.js
@@ -0,0 +1,16 @@
+// input: số
+// output: kết luận âm hay dương?
+
+let number = 0;
+
+if(number < 0){
+    console.log("Số âm!");
+}
+
+if(number == 0){
+    console.log("Không âm không dương!")
+}
+
+if(number > 0){
+    console.log("Số dương!")
+}
---
+++ day8/main.js
@@ -0,0 +1,43 @@
+/*
+ Toán tử số học
+ */
+
+let a = 2;
+let b = 5;
+
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a : b = ", a / b);
+console.log("a ^ b = ", a ** b);
+console.log("a % b = ", a % b);
+
+/*
+TOÁN TỬ SO SÁNH >, <, >=, <= , == 
+KẾT QUẢ: BOOLEAN (TRUE HOẶC FALSE)
+*/
+
+console.log("Toán tử so sánh - quan hệ");
+
+console.log("a > b: ", a > b);
+console.log("a < b: ", a < b);
+console.log("a <= b: ", a <= b);
+console.log("a >= b: ", a >= b);
+console.log("a == b: ", a == b);
+console.log("a != b", a != b);
+
+// ===: phải cùng giá trị và cùng loại
+// true (1), false (0)
+console.log(1 === true)
+// 1.0 và 1
+
+
+/*
+TOÁN TỬ LOGIC: AND  (VÀ), OR (HOẶC), NOT (PHỦ ĐỊNH)
+VD: nếu trời không mưa và có lịch học thì học thể dục bình thường.
+    (Thời tiết != mưa) && (lichHoc == true)
+
+VD: Nếu trời mưa to hoặc có tuyết -> học thể dục trong nhà.
+   (thời tiết == mưa to) || (có tuyết == true)
+
+*/
