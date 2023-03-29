a3fc66d96d5c7ff91eb2182e336f28465136cbbb
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-29 20:03:30 +0700

    update day10

---
+++ day10/main.js
@@ -0,0 +1,8 @@
+// In ra các giá trị từ 1 đến 10
+// i ++ <=>  i = i + 1
+for (let i = 1; i <= 10; i++) {
+  console.log("i =", i);
+}
+// In ra cac gia tri 1-> 10, nam trong the p
+// <p>1</p>
+// <p>2</p>
