20e2e47eb73a4feb51cb4c99cb5457b8d8b1b857
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-04-21 20:55:53 +0700

    day19

---
+++ day19/src/components/Box.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./Box.css";
+export function Box(props){
+
+    // Mặc đinh state color -> Giống màu ngừoi dùng truyền vào
+    const [stateColor, setColor] = useState(props.color);
+
+    function changeColor(){
+        if(stateColor === "white"){
+            setColor("black");
+        }
+        else{
+            setColor("white");
+        }
+    }
+
+    return <div onClick={changeColor} className="box" 
+    style={{backgroundColor: stateColor}}>
+        
+    </div>
+}
+// Mau mac dinh la mau trang
+Box.defaultProps = {
+    color:"white",
+}
