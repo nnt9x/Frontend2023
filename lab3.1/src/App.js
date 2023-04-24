6f5bd830f291c4450fb5822373cd7cfbe6ed895a
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-04-24 21:28:56 +0700

    update App.js

--- lab3.1/src/App.js
+++ lab3.1/src/App.js
@@ -1,20 +1,19 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
-import { Dice } from './components/Dice';
 import { NewDice } from './components/NewDice';
 
 function App() {
 
-  const [value, setValue] = useState(0);
+  // const [value, setValue] = useState(0);
 
-  function randomDice(){
-     const rd = Math.floor(Math.random() * 6) + 1;
-     setValue(rd);
-  }
+  // function randomDice(){
+  //    const rd = Math.floor(Math.random() * 6) + 1;
+  //    setValue(rd);
+  // }
 
   return (
     <div className="App">
-      <div onClick={randomDice} style={{width:"400px"}}>
+      <div style={{width:"400px"}}>
         {/* <p style={{color:"red", fontSize:"40px"}}>{value}</p>
         <Dice value={"" + value}/> */}
         
