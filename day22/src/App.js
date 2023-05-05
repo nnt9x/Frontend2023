3bc912d6b9106cad31a20e0bd83729a4e19071d3
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-05-05 20:37:45 +0700

    day22 list

--- day22/src/App.js
+++ day22/src/App.js
@@ -1,19 +1,34 @@
-import './App.css';
-import data from './animals.json';
-import { Animal } from './components/Animal';
+import { useState } from "react";
+import "./App.css";
+import data from "./animals.json";
+import { Animal } from "./components/Animal";
 
 function App() {
-  // Đọc dữ liệu từ file JSON
-  const animals = data.animals;
-  // Sử dụng hàm map -> biến đổi dữ liệu -> JSX (component)
-  const listAnimals = animals.map((animal) => <Animal key={animal.id} name={animal.name} src={animal.src}/>)
+	const [name, setName] = useState("Chưa chọn con vật");
 
-  return (
-    <div className="container">
-      {/* Render ra dc 1 list -> phu thuoc vao so luong du lieu dang co */}
-      {listAnimals}
-    </div>
-  );
+	function selectAnimal(newName) {
+		setName(newName);
+	}
+
+	// Đọc dữ liệu từ file JSON
+	const animals = data.animals;
+	// Sử dụng hàm map -> biến đổi dữ liệu -> JSX (component)
+	const listAnimals = animals.map((animal) => (
+		<Animal
+			callBackApp={selectAnimal}
+			key={animal.id}
+			name={animal.name}
+			src={animal.src}
+		/>
+	));
+
+	return (
+		<div className="container">
+			<p className="display-1">Tên con vật được chọn - {name}</p>
+			{/* Render ra dc 1 list -> phu thuoc vao so luong du lieu dang co */}
+			{listAnimals}
+		</div>
+	);
 }
 
 export default App;
--- day22/src/components/Animal.jsx
+++ day22/src/components/Animal.jsx
@@ -2,8 +2,7 @@
 import "./Animal.css";
 export function Animal(props) {
 	return (
-		<div className="animal">
-			
+		<div className="animal" onClick={()=> props.callBackApp(props.name)}>
 			<img className="img-fluid" src={props.src} 
             alt={props.name} />
             <p>{props.name}</p>
