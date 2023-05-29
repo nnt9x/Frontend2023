9a4034ebb2c760c63e30ec6fccd7252b5da474bd
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-05-29 20:20:43 +0700

    update code phân trang

--- lab_react_bkacad-main/src/App.js
+++ lab_react_bkacad-main/src/App.js
@@ -9,33 +9,63 @@ import { UserPage } from "./pages/UserPage";
 
 function App() {
 	const [users, setUsers] = useState([]);
-	const limit = 12;
+
+	const [page, setPage] = useState(1);
+
+	const limit = 45;
+
+	const [total_page, setTotalPage] = useState(-1);
+
+	let skip = (page - 1) * limit;
 
 	useEffect(() => {
-		fetch(`https://dummyjson.com/users?limit=${limit}`)
+		fetch(`https://dummyjson.com/users?limit=${limit}&skip=${skip}`)
 			.then((response) => response.json())
 			.then((data) => {
 				const newUser = data.users;
 				const sortedUser = newUser.sort((a, b) => {
-					if(a.firstName < b.firstName) { return -1; }
-					else if(a.firstName > b.firstName) { return 1; }
-					else { return 0; }
+					if (a.firstName < b.firstName) {
+						return -1;
+					} else if (a.firstName > b.firstName) {
+						return 1;
+					} else {
+						return 0;
+					}
 				});
 				setUsers(sortedUser);
+
+				setTotalPage(Math.ceil(parseFloat(data.total) / limit));
 			})
 			.catch((error) => console.log(error));
-	}, []);
+	}, [skip]);
 
-	function searchUser(kw){
+	function searchUser(kw) {
 		fetch(`https://dummyjson.com/users/search?q=${kw}&limit=${limit}`)
-		.then((response) => response.json())
-		.then((data) => setUsers(data.users))
-		.catch((error) => console.log(error));
+			.then((response) => response.json())
+			.then((data) => setUsers(data.users))
+			.catch((error) => console.log(error));
+	}
+
+	function nextPreviousPage(c) {
+		if (c === "next") {
+			const newPage = page + 1;
+			// Page co vuot qua total page hay ko?
+			if (newPage <= total_page) {
+				setPage(newPage);
+			}
+		} else if (c === "previous") {
+			const newPage = page - 1;
+			// Page toi thieu bang 1
+			if (newPage >= 1) {
+				setPage(newPage);
+			}
+		}
 	}
 
 	return (
 		<BrowserRouter>
-			<AppContext.Provider value={{ users, searchUser }}>
+			<AppContext.Provider
+				value={{ users, searchUser, nextPreviousPage, page }}>
 				<div className="app container">
 					<Header />
 					{/* Content */}
--- lab_react_bkacad-main/src/pages/UserPage.jsx
+++ lab_react_bkacad-main/src/pages/UserPage.jsx
@@ -2,13 +2,16 @@ import { useContext } from "react";
 import { ListViewUser } from "../components/ListViewUser";
 import { AppContext } from "../context/AppContext";
 
-export function UserPage(props){
+export function UserPage(){
 
-    const {users} = useContext(AppContext);
+    const {users, nextPreviousPage, page} = useContext(AppContext);
 
     return (
         <div>
             <ListViewUser users={users}/>
+            <button onClick={()=> nextPreviousPage("previous")} className="btn btn-secondary mx-2">Prev</button>
+            <b>{page}</b>
+            <button onClick={()=> nextPreviousPage("next")} className="btn btn-primary mx-2">Next</button>
         </div>
     )
 }
